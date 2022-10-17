10b4a095806cdd4ea7a30a115cf626f442df94c2
e1b20023 <e1b20023@oit.ac.jp>
2022-10-17 15:01:44 +0900

    入室管理の実装2

--- src/main/java/oit/is/z0481/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0481/kaizi/janken/controller/JankenController.java
@@ -8,17 +8,19 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z0481.kaizi.janken.model.Entry;
 
 @Controller
+@RequestMapping("/")
 public class JankenController {
 
   @Autowired
   private Entry room;
 
-  @GetMapping("/janken")
+  @GetMapping("janken")
   public String janken(Principal prin, ModelMap model) {
     String loginUser = prin.getName();
     this.room.addUser(loginUser);
@@ -27,13 +29,13 @@ public class JankenController {
     return "janken.html";
   }
 
-  @PostMapping("/janken")
+  @PostMapping("janken")
   public String janken(@RequestParam String name, ModelMap model) {
     model.addAttribute("name", name);
     return "janken.html";
   }
 
-  @GetMapping("/choice/{param1}")
+  @GetMapping("choice/{param1}")
   public String choice(@PathVariable String param1, ModelMap model) {
     if (param1.equals("グー")) {
       model.addAttribute("gu", param1);
